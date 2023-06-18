d1581d880cafa1f9c3a3c72beec6f0998c5e8a67
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-06-17 19:17:18 -0500

    ejemplo de directivas y pipes.

--- src/app/ejemplos/directivas-demo/directivas-demo.component.ts
+++ src/app/ejemplos/directivas-demo/directivas-demo.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./directivas-demo.component.scss']
 })
 export class DirectivasDemoComponent {
+  seccionActiva = 0;
+  alumnos = [
+    {nombre: "Luis Angel"},
+    {nombre: "Jesús Pedro"},
+    {nombre: "Wilfredo"},
+    {nombre: "Luis Gabriel"}
+  ];
+  cursos = [
+    {nombre: "Desarrollo de aplicaciones Web 1", precio: 3500},
+    {nombre: "Base de datos", precio: 2500},
+    {nombre: "Gestión de Proyectos", precio: 1500},
+    {nombre: "Desarrollo de aplicaciones Web 2", precio: 4500},
+    {nombre: "Desarrollo de aplicaciones móviles", precio: 3000}
+  ];
+  currentDate = new Date();
+
+  mostrarAlumnos(): void{
+    this.seccionActiva = 0;
+  }
+  mostrarCursos(): void {
+    this.seccionActiva = 1;
+  }
 
 }
